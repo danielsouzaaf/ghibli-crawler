a578e626e1857091f5c1968b55f80a8c9f89fa06
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2019-06-29 17:21:08 -0300

    factory de filmes

---
+++ database/factories/MovieFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+$factory->define(\GhibliCrawler\Models\Movie::class, function (Faker $faker) {
+    return [
+        'id' => $faker->slug,
+        'title' => $faker->title,
+        'description' => $faker->paragraphs,
+        'director' => $faker->name,
+        'producer' => $faker->name,
+        'release_date' => $faker->year,
+        'rt_score' => $faker->numberBetween(0, 100),
+        'url' => $faker->url,
+    ];
+});
