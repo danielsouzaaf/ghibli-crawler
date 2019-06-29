7d96157cffcac2bf4c3e7e0392ade268338c5a77
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2019-06-29 13:21:45 -0300

    definição do modelo de filmes

---
+++ app/Models/Movie.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace GhibliCrawler\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "type" of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /*
+     * The attributes that should be mass assignable.
+     */
+    protected $fillable = [
+        'id', 'title', 'description', 'director', 'producer', 'release_date', 'rt_score'
+    ];
+
+
+}
