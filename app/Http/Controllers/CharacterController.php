eb998dda5801450629b808a12696791a97c71932
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2019-06-29 18:56:40 -0300

    retornando baseado no tipo json

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -2,6 +2,7 @@
 
 namespace GhibliCrawler\Http\Controllers;
 
+use GhibliCrawler\Presenters\CharacterPresenter;
 use GhibliCrawler\Repositories\CharacterRepository;
 use GhibliCrawler\Repositories\MovieRepository;
 use Illuminate\Http\Request;
@@ -11,17 +12,19 @@ class CharacterController extends Controller
 {
     private $characterRepository;
     private $movieRepository;
+    private $presenter;
 
-    public function __construct(CharacterRepository $characterRepository, MovieRepository $movieRepository)
+    public function __construct(CharacterRepository $characterRepository, MovieRepository $movieRepository, CharacterPresenter $presenter)
     {
         $this->characterRepository = $characterRepository;
         $this->movieRepository = $movieRepository;
+        $this->presenter = $presenter;
     }
 
     public function results(Request $request)
     {
         $characters = $this->characterRepository->allWithMovies();
 
-        return Response::json($characters);
+        return $this->presenter->present($characters, CharacterPresenter::FORMATS_ARRAY[$request->input('fmt')]);
     }
 }
--- app/Presenters/CharacterPresenter.php
+++ app/Presenters/CharacterPresenter.php
@@ -5,27 +5,57 @@ namespace GhibliCrawler\Presenters;
 
 
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Response;
 
 class CharacterPresenter
 {
+    const FORMAT_CSV = 'csv';
+    const FORMAT_JSON = 'json';
+    const FORMAT_HTML = 'html';
+
+    const FORMATS_ARRAY = [
+        self::FORMAT_JSON => self::FORMAT_JSON,
+        self::FORMAT_CSV => self::FORMAT_CSV,
+        self::FORMAT_HTML => self::FORMAT_HTML,
+    ];
+
     public function present(Collection $characters, $format)
     {
+        $mapped = $characters->map(function($character) {
+            return [
+                'nome' => $character->name,
+                'idade' => $character->age,
+                'filmes' => $character->movies->map(function ($movie) {
+                    return [
+                        'titulo' => $movie->title,
+                        'ano_lancamento' => $movie->release_date,
+                        'pontuacao_rotten_tomato' => $movie->rt_score
+                    ];
+                })
+            ];
+        });
+
+        if ($format == self::FORMAT_JSON)
+            return $this->outputAsJson($mapped);
+        else if ($format == self::FORMAT_CSV)
+            return $this->outputAsCsv($mapped);
+        return $this->outputAsHtml($mapped);
 
     }
 
-    private function outputAsCsv()
+    private function outputAsCsv($mapped)
     {
-
+        //TODO: Implementar CSV
     }
 
-    private function outputAsJson()
+    private function outputAsJson($mapped)
     {
-
+        return Response::json($mapped);
     }
 
-    private function outputAsHtml()
+    private function outputAsHtml($mapped)
     {
-
+        //TODO: Implementar HTML
     }
 
 }
