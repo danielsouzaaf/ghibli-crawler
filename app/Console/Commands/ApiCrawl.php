4eb36c31daf0ef2d6cadb4cdd46bf089f04c3af0
Daniel Souza Affonso Ferreira <daniel.souza.af@gmail.com>
2019-06-29 13:10:58 -0300

    definição do comando

---
+++ app/Console/Commands/ApiCrawl.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace GhibliCrawler\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ApiCrawl extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'api:crawl';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Obter e armazenar as informações da API do Studio Ghibli.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+    }
+}
